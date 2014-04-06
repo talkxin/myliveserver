ea922363f7754ee917be2a75d0f5565b533263d1
talkliu <talkliu@talkliu-PC>
2014-04-06 18:07:30 +0800

    添加datajson字段

--- src/main/resources/家庭银行.sql
+++ src/main/resources/家庭银行.sql
@@ -1,37 +1,13 @@
 /*
 MySQL Data Transfer
-Source Host: localhost
+Source Host: 192.168.174.130
 Source Database: mylive
-Target Host: localhost
+Target Host: 192.168.174.130
 Target Database: mylive
-Date: 2014/4/4 16:48:14
+Date: 2014/4/6 18:06:23
 */
 
 SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for t_paybank_card
--- ----------------------------
-DROP TABLE IF EXISTS `t_paybank_card`;
-CREATE TABLE `t_paybank_card` (
-  `id` int(19) NOT NULL AUTO_INCREMENT,
-  `uid` int(19) DEFAULT NULL,
-  `cardNo` varchar(10) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for t_paybank_home
--- ----------------------------
-DROP TABLE IF EXISTS `t_paybank_home`;
-CREATE TABLE `t_paybank_home` (
-  `id` int(19) NOT NULL AUTO_INCREMENT,
-  `uid` int(19) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `pinyinName` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 -- ----------------------------
 -- Table structure for t_paybank_shouzhi
 -- ----------------------------
@@ -46,18 +22,7 @@ CREATE TABLE `t_paybank_shouzhi` (
   `cardName` int(19) DEFAULT NULL,
   `applyto` int(19) DEFAULT NULL,
   `note` varchar(2000) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for t_paybank_zhichutype
--- ----------------------------
-DROP TABLE IF EXISTS `t_paybank_zhichutype`;
-CREATE TABLE `t_paybank_zhichutype` (
-  `id` int(19) NOT NULL AUTO_INCREMENT,
-  `uid` int(19) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `pinyinName` varchar(500) DEFAULT NULL,
+  `datajson` varchar(5000) DEFAULT NULL COMMENT '图片或者语音的json保存位置',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
